df10ba1e5642b570781650b5fc997c98d7569962
Alexey Teterin <teterin.alexey@gmail.com>
2021-02-19 22:46:13 +0500

    feat: add Board, Square, materialize.css

---
+++ src/Square.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Square() {
+  return (
+    <button type="button" className="square brown lighten-4">
+      {/* TODO */}
+    </button>
+  );
+}
